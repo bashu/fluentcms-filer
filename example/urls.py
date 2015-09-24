a554dc4c7f292b918161f1c5ea609d3d14fd41d2
Basil Shubin <basil.shubin@gmail.com>
2015-09-24 08:31:24 +0600

    updated ``example`` project

--- example/urls.py
+++ example/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tinymce/', include('tinymce.urls')),                       
 )
 
 if settings.SERVE_MEDIA:
